6b349ca96bc26c39054e49101714e46eed2845da
David Tucker <david@davidtucker.net>
2021-08-19 20:38:32 -0400

    Updating install script

--- install.sh
+++ install.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
 # Install zsh
-apt install zsh wget
-chsh -s $(which zsh)
+apt install -y zsh wget fontconfig
+chsh -s /usr/bin/zsh node
 
 # Install fonts
-mkdir ~/.fonts
-wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -P ./fonts/
-wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf -P ./fonts/
-wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf -P ./fonts/
-wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf -P ./fonts/
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -P /usr/share/fonts
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf -P /usr/share/fonts
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf -P /usr/share/fonts
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf -P /usr/share/fonts
+
 # Rebuild font cache
 fc-cache -f -v
 
-# Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 # Install Powerlevel10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >> ~/.zshrc
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/node/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> /home/node/.zshrc
 
 # Install default config file
-wget https://raw.githubusercontent.com/davidtucker/setup-shell/main/.p10k.zsh -P ./
-
+wget https://raw.githubusercontent.com/davidtucker/setup-shell/main/.p10k.zsh -P /home/node/
+echo '[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' >> /home/node/.zshrc
